71f0b2912f092a1c3ced1c756e362051217402fd
pg-kkn1125 <kkn@pg-ander.com>
2023-04-21 23:08:15 +0900

    init: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LIVE COMMERCE VERSION 2
+
+제 2 라이브커머스 환경 구축.
---
+++ front/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+import path from "path";
+
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  dotenv.config({
+    path: path.join(path.resolve(), ".env"),
+  });
+  dotenv.config({
+    path: path.join(path.resolve(), `.env.${mode}`),
+  });
+
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+    server: {
+      host: process.env.HOST,
+      port: Number(process.env.PORT) || 3000,
+    },
+    plugins: [react()],
+  };
+});
